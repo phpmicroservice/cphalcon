77dc5afc03a1df598480b5e483202b5bec48e543
dongasai <1514582970@qq.com>
2019-11-12 15:04:14 +0800

    升级版本号

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 # cd /home/cphalcon;/home/zephir-0.12.11/zephir fullclean;
 /home/zephir-0.12.11/zephir build
+/home/zephir-0.12.11/zephir stubs
 cd /home/cphalcon/build;php gen-build.php
 ./install
